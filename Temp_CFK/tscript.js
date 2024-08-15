fd6b205e74ad52c6b018238ef88c221f2f8d6d8c
martcos25 <m.santana.corsino@gmail.com>
2024-08-15 18:14:19 -0400

    Added Formulas and Constants

---
+++ Temp_CFK/tscript.js
@@ -0,0 +1,11 @@
+//Celsius --> Farenheit = [(9*C)/5] + 32
+//Farenheit --> Celsius = [(F-32)*5] /9
+//Celsius --> Kelvin = C + 273
+//Kelvin --> Celsius = K - 273
+//Farenheit --> Kelvin = {[(F-32) * 5] /9 } + 273
+//Kelvin --> Farenheit = {[(K-273) * 9] /5} + 32
+
+//Cero Absoluto
+const kz = 273;
+const fz = 32;
+const cz = 0
