e26bff0671fbad693e824e20d4987380c83f1239
martcos25 <m.santana.corsino@gmail.com>
2024-08-20 09:09:53 -0400

    Added Raw Student Data and bits of style

--- Grades/script.js
+++ Grades/script.js
@@ -2,3 +2,15 @@
 //Funcion para agregar y quitar calificaciones
 //Calcular promedio y comparar con el mismo
 //Ordenar calificaciones
+
+let students = [
+  {  id: 1,  name: "Wilfredo Small",  grades: [100, 70, 80, 67, 87, 95, 92, 75, 65, 90]},
+  {  id: 2,  name: "Stuart Vang",  grades: [95, 84, 75, 95, 99, 100, 67, 77, 81, 69]},
+  {  id: 3,  name: "Benita Miranda",  grades: [75, 80, 90, 92, 96, 80, 84, 88, 70, 75]},
+  {  id: 4,  name: "Dannie Dixon",  grades: [80, 50, 70, 61, 40, 74, 80, 72, 90, 100]},
+  {  id: 5,  name: "Merlin Gillespie",  grades: [95, 90, 91, 94, 70, 70, 75, 60, 70, 90]},
+  {  id: 6,  name: "Bill Horn",  grades: [100, 70, 80, 67, 87, 95, 92, 75, 65, 90]},
+  {  id: 7,  name: "Barton Fry",  grades: [100, 70, 80, 67, 87, 95, 92, 75, 65, 90]},
+  {  id: 8,  name: "Doreen Bowman",  grades: [80, 75, 84, 70, 84, 90, 84, 70, 65, 90]},
+  {  id: 9,  name: "Hannah Peters",  grades: [90, 75, 80, 67, 87, 95, 92, 75, 61, 50]},
+]
