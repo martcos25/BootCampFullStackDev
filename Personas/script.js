7923cc33535527aa05cb63eb488c269bc2e4fb8d
martcos25 <m.santana.corsino@gmail.com>
2024-08-22 23:08:09 -0400

    Added Base Methods and Comments

--- Personas/script.js
+++ Personas/script.js
@@ -1,7 +1,25 @@
+//Clase de Personas con propiedades de nombre, edad e intereses
 class Person {
   constructor(name, age, interest) {
     this.name = name;
     this.age = age;
     this.interest = interest;
+    //interest sera un array de strings
+  }
+  //Metodo para añadir interes
+  addInterest(interest) {
+
+  }
+  //Metodo para borrar interes
+  deleteInterest(interest) {
+
+  }
+  //Metodo para formatear intereses
+  showInterests(interest) {
+
+  }
+  //Metodo para mostrar toda la informacion 
+  showInfo(interest) {
+
   }
 }
