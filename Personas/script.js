38472b4de8dede3cf8b625fe247a8000316288f1
martcos25 <m.santana.corsino@gmail.com>
2024-08-27 13:50:16 -0400

    Removed Method, Back to 1

--- Personas/script.js
+++ Personas/script.js
@@ -4,35 +4,9 @@ const add_last = document.getElementById('add-last')
 const del_first = document.getElementById('del-first')
 const del_last = document.getElementById('del-last')
 
-
-let intlist = document.getElementById('intlist')
-let inter = document.getElementById('new-inter')
-
-//Clase de Personas con propiedades de nombre, edad e intereses
 class Person {
-  constructor(name, age, interest=[]) {
+  constructor(name, age) {
     this.name = name;
     this.age = age;
-    this.interest = interest;
-    //interest sera un array de strings
-  }
-  //Metodo para añadir interes
-  addInterest() {
-
-  }
-  //Metodo para borrar interes
-  deleteInterest(interest) {
-    return interest.pop()
-  }
-  //Metodo para formatear intereses
-  showInterests(interest) {
-
-  }
-  //Metodo para mostrar toda la informacion 
-  showInfo() {
-    
   }
 }
-
-let someone = new Person("John Wilson", 50 )
-console.log(someone.name);
