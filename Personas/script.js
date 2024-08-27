5477b1b08c7731d266f77da0d2b08c8838475e6a
martcos25 <m.santana.corsino@gmail.com>
2024-08-27 15:57:58 -0400

    Fixed + String Interpolation

--- Personas/script.js
+++ Personas/script.js
@@ -4,9 +4,23 @@ const add_last = document.getElementById('add-last')
 const del_first = document.getElementById('del-first')
 const del_last = document.getElementById('del-last')
 
+const nameEl = document.getElementById("person")
+const ageEl = document.getElementById("age")
+
 class Person {
-  constructor(name, age) {
+  constructor(name,  age, interest=[]) {
     this.name = name;
     this.age = age;
+    this.interest = interest
+  }
+  addInterest() {
+    checkMessage()  
+  }
+  removeInterest() {
+    return this.interest.pop()
   }
 }
+
+let Person1 = new Person("Johnathan Fernandez", 45, []);
+nameEl.innerHTML = "Nombre: " + Person1.name
+ageEl.innerHTML = `Edad: ${Person1.age} años`
